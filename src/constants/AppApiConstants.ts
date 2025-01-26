622170186dae21dd0b6a6536ca7bcc3c46469ebf
Mohammed Hattab <mohammedhattab97@gmail.com>
2025-01-25 19:17:25 -0500

    added some comments

--- src/constants/AppApiConstants.ts
+++ src/constants/AppApiConstants.ts
@@ -5,6 +5,8 @@ import React from "react";
 class ApiConstants {
 
     static readonly TRAVEL_TRACKER_API_URL: string = 'https://travel-tracker-api-tvan66p23a-uc.a.run.app/';
+    // static readonly TRAVEL_TRACKER_API_URL: string = 'http://192.168.1.196:5000/';
+
 
 }
 
