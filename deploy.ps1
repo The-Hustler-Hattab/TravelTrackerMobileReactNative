72400c05771c2c74b63bb40cf42a2318572bcb88
Mohammed Hattab <mohammedhattab97@gmail.com>
2025-01-26 19:49:49 -0500

    clean up

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,7 @@
-# android build
-# eas build -p android --profile preview
+# android build test
+# eas build -p android --profile preview --clear-cache
+# android build production
+# eas build -p android --profile production --clear-cache
+
 
 eas update
